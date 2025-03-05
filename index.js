aa5d56bac1e9fa2ce81bc977d8a0de8781922b79
Ahlyab <ahlyabasad@gmail.com>
2025-03-05 23:19:14 +0500

    cors added

--- index.js
+++ index.js
@@ -4,8 +4,14 @@ require("dotenv").config();
 const TodoRoutes = require("./routes/todoRoutes");
 const app = express();
 const port = process.env.PORT || 3000;
+const cors = require("cors");
 
 // middleware
+app.use(
+  cors({
+    origin: "*",
+  })
+);
 app.use(express.json());
 
 // routes
